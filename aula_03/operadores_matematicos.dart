09d842ac76f17fa5968ababab365143a5d65908d
Leandro <leandrodbdias@gmail.com>
2020-03-23 13:34:03 -0300

    Operadores matemáticos

---
+++ aula_03/operadores_matematicos.dart
@@ -0,0 +1,18 @@
+main() {
+  int valor_01 = 10;
+  int valor_02 = 5;
+
+  int totalAdicao = valor_01 + valor_02;
+  int totalSubtracao = valor_01 - valor_02;
+  int totalMultiplicacao = valor_01 * valor_02;
+  int restoDivisao = valor_01 % valor_02;
+  double totalDivisao = valor_01 / valor_02;
+  double ordemProcedencia = (1 * 3) + (4 / 3 + ((4 + 4) * 3));
+
+  print(totalAdicao);
+  print(totalSubtracao);
+  print(totalMultiplicacao);
+  print(totalDivisao);
+  print(restoDivisao);
+  print(ordemProcedencia);
+}
