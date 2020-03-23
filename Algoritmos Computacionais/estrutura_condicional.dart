6ba8575eae99a0d87744ea877468ed7e848b7b78
Leandro <leandrodbdias@gmail.com>
2020-03-23 13:49:14 -0300

    estrutura condicionais IF, ELSE

---
+++ Algoritmos Computacionais/estrutura_condicional.dart
@@ -0,0 +1,12 @@
+main() {
+  int idade_aluno = 18;
+
+  if (idade_aluno > 12)
+    print("Aluno menor da idade, requer responsável");
+
+  else if (idade_aluno >= 18)
+    print("Aluno maior de idade");
+
+  else
+    print("Aluno menor de idade");
+}
