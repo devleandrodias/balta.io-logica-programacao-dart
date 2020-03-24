ae3613a8f751984a23f98a667ec2c0d4eaf468b0
Leandro <leandrodbdias@gmail.com>
2020-03-24 20:23:57 -0300

    Tipagem

---
+++ Sintaxes/tipagem.dart
@@ -0,0 +1,13 @@
+main(List<String> args) {
+  int valorInteiro = 0;
+  double valorDecimal = 1.0;
+  bool valorBooleano = true;
+  String valorTexto = "Texto";
+  var tipagemVariada = "123ABV";
+
+  print(valorInteiro);
+  print(valorDecimal);
+  print(valorBooleano);
+  print(valorTexto);
+  print(tipagemVariada);
+}
