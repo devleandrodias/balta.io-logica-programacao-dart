c677695c4e255228d10a1d674345d3c7660faaf1
Leandro <leandrodbdias@gmail.com>
2020-04-21 12:57:56 -0300

    Finalização do módulo carrinho de compras

--- Programa - Lista de Compras/carrinho_compras.dart
+++ Programa - Lista de Compras/carrinho_compras.dart
@@ -12,20 +12,25 @@ main() {
       print("====== Terminou o programa ======");
       condicao = false;
     } else if (text == "imprimir") {
-      for (var i = 0; i < produtos.length; i++) {
-        print("Item $i - ${produtos[i]}");
-      }
+      imprimir(produtos);
     } else if (text == "remover") {
       print("====== Qual item deseja remover? ======");
-      for (var i = 0; i < produtos.length; i++) {
-        print("Item $i - ${produtos[i]}");
-      }
-
-      int item = int.parse(stdin.readLineSync());
-      produtos.removeAt(item);
+      imprimir(produtos);
+      remover(produtos);
     } else {
       produtos.add(text);
       print("\x1B[2J\x1B[0;0H");
     }
   }
+}
+
+imprimir(List<String> produtos) {
+  for (var i = 0; i < produtos.length; i++) {
+    print("Item $i - ${produtos[i]}");
+  }
+}
+
+remover(List<String> produtos) {
+  int item = int.parse(stdin.readLineSync());
+  produtos.removeAt(item);
 }
