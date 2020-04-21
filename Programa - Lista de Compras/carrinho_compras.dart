b93949ed8179a89b711b2aacf9fc6a16d5cb2a5e
Leandro <leandrodbdias@gmail.com>
2020-04-21 12:49:25 -0300

    Remover um item da lista

--- Programa - Lista de Compras/carrinho_compras.dart
+++ Programa - Lista de Compras/carrinho_compras.dart
@@ -15,6 +15,14 @@ main() {
       for (var i = 0; i < produtos.length; i++) {
         print("Item $i - ${produtos[i]}");
       }
+    } else if (text == "remover") {
+      print("====== Qual item deseja remover? ======");
+      for (var i = 0; i < produtos.length; i++) {
+        print("Item $i - ${produtos[i]}");
+      }
+
+      int item = int.parse(stdin.readLineSync());
+      produtos.removeAt(item);
     } else {
       produtos.add(text);
       print("\x1B[2J\x1B[0;0H");
