d76cd501fbce0fe22c91192a09d0262d29bd1ca5
Leandro <leandrodbdias@gmail.com>
2020-04-21 12:08:50 -0300

    Estrutura inicial do carrinho de compras

---
+++ Programa - Lista de Compras/carrinho_compras.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+main() {
+  List<String> produtos = [];
+  bool condicao = true;
+
+  while (condicao) {
+    print("====== Adicione um novo produto ======");
+    String text = stdin.readLineSync();
+
+    if (text == "sair") {
+      print("====== Terminou o programa ======");
+      condicao = false;
+    } else if (text == "imprimir") {
+      print(produtos);
+      print("\n");
+    } else {
+      produtos.add(text);
+    }
+  }
+}
